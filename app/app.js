cb55156d872d94aeadf1caa63d09f2389afe7ecd
remony <sudouglas@dundee.ac.uk>
2015-09-15 21:46:41 +0100

    Modified base template to work and implemented basic hamburger to demonstrate it. Removed un-required files and renamed view1 to home

--- app/app.js
+++ app/app.js
@@ -1,12 +1,18 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+var App = angular.module('myApp', [
+    'ngRoute',
+    'myApp.home',
+    'myApp.version',
+    'angular-loading-bar',
+    'ngHamburger'
+]);
+
+App.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.otherwise({
+            redirectTo: '/view1'
+        });
+    }
+]);
