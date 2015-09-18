52f227be69f3353de3d3586b2041329e8127bd15
remony <sudouglas@dundee.ac.uk>
2015-09-18 13:28:45 +0100

    Added call to get site information to populate meta data

--- app/app.js
+++ app/app.js
@@ -28,16 +28,40 @@ App.config(['$routeProvider',
     }
 ]);
 
+App.controller('appCtrl', ['$scope', '$http', function($scope, $http) {
+    console.log("Page is loading");
+    $http({
+             url: "http://localhost:8080/",
+             method: 'GET',
+             dataType: 'json',
+             data: '',
+             headers: {
+                 'Content-Type': 'application/json; charset=utf-8',
+             }
+                }).success(function(data, status, headers, config) {
+
+                $scope.data = data;
+                console.log(data);
+
+            }).
+            error(function(data, status, headers, config) {
+                $scope.error = true;
+                
+                
+                
+            });
+}]);
+
+
+
 App.controller('navigationCtrl',  ['$scope', 'localStorageService', '$rootScope',     function($scope, localStorageService, $rootScope) {
 
     function resetMenu() {
         $scope.items = {"items": [{"title": "Home", "link": '#/home'}, {"title": "News", "link": "#/news"}, {"title": "Admin", "link": "#/admin", "subitems": [{"title":"Admin Panel", "link": "#/users"},{"title":"Users", "link": "#/users"}]}]};
-        
     }
     
     resetMenu();
-    
-    //console.log($scope.items);
+
 
     var isAuthed = localStorageService.get('loggedIn');
 
