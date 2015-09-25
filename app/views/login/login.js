22b78a2912b473498dc376b5275f93f0bc8d1aa5
remony <sudouglas@dundee.ac.uk>
2015-09-25 11:16:28 +0100

    Issues fixed, started on page editor

--- app/views/login/login.js
+++ app/views/login/login.js
@@ -8,6 +8,8 @@ angular.module('app.login', ['ngRoute'])
             templateUrl: 'views/login/login.html',
             controller: 'loginCtrl'
         });
+
+
     }
 ])
 
@@ -15,7 +17,7 @@ angular.module('app.login', ['ngRoute'])
     function($routeProvider) {
         $routeProvider.when('/logout', {
             templateUrl: 'views/login/login.html',
-            controller: 'loginCtrl'
+            controller: 'logoutCtrl'
         });
     }
 ])
@@ -23,36 +25,37 @@ angular.module('app.login', ['ngRoute'])
     function($scope, $http, localStorageService, $rootScope, $location) {
         $scope.title = "Login";
         $scope.error = false;
-		var auth = localStorageService.get('user_auth');
+        var auth = localStorageService.get('user_auth');
 
 
 
-		console.log(auth);
+        console.log(auth);
         if (auth != null) {
-        	$scope.loggedIn = true;
-        	console.log(auth.user_auth[0].username);
-        	$scope.username = auth.user_auth[0].username;
+            $scope.loggedIn = true;
+            console.log(auth.user_auth[0].username);
+            $scope.username = auth.user_auth[0].username;
+              $location.path( "#/" );
         } else {
-        	$scope.loggedIn = false;
+            $scope.loggedIn = false;
         }
 
         
         
 
         $scope.logout = function() {
-			console.log("logging out");
-			localStorageService.remove('user_auth');
-			$scope.loggedIn = false;
+            console.log("logging out");
+            localStorageService.remove('user_auth');
+            $scope.loggedIn = false;
             localStorageService.set('loggedIn', false);
             $rootScope.$emit('loginStatus', false);
             //toastr.info('Logout Success', 'You have logged out :(');
-		}
+        }
 
 
         $scope.userlogin = function(username, password) {
 
-	
-		 $http({
+    
+         $http({
          url: backend + "/user/login",
          method: 'POST',
          dataType: 'json',
@@ -62,30 +65,52 @@ angular.module('app.login', ['ngRoute'])
              'username':username.username,
              'password':username.password
          }
-			}).success(function(data, status, headers, config) {
+            }).success(function(data, status, headers, config) {
                 
 
                // document.cookie("token=" + data.user_auth.token);
-			localStorageService.set('user_auth', data);
+            localStorageService.set('user_auth', data);
             localStorageService.cookie.set("username",data.user_auth.username,10);
             //$cookies.put("token", data.user_auth.token);
             localStorageService.cookie.set("usergroup",data.user_auth.usergroup,10);
             localStorageService.cookie.set("token",data.user_auth.token,10);
-			$scope.loggedIn = true;
-			$scope.error = false;
+            // $scope.loggedIn = true;
+            Materialize.toast("Login Success, Welcome " + username.username, 1000);
+
+            // $scope.error = false;
             //$location.path( "#/" );
             // toastr.success('Login Success', 'You have logged in!');
-            $rootScope.$emit('loginStatus', true);
-		}).
-		error(function(data, status, headers, config) {
-            toastr.error('Login Failed', 'Something went wrong...');
-			$scope.error = true;
-            $scope.$emit('loginStatus', false);
-			$rootScope.loggedIn = false;
-			
-		});
+            // $scope.$emit('loginStatus', true);
+            $location.path( "" );
+            location.reload();
+        }).
+        error(function(data, status, headers, config) {
+            Materialize.toast("Login Failed", 1000);
+            $scope.error = true;
+            // $scope.$emit('loginStatus', false);
+            // $rootScope.loggedIn = false;
+            
+        });
 
 
 
-	}
+    }
+}])
+.controller('logoutCtrl', ['$scope', '$http', 'localStorageService', '$rootScope', '$location',
+    function($scope, $http, localStorageService, $rootScope, $location) {
+        $scope.title = "Logout";
+       
+
+        
+            // console.log("logging out");
+            localStorageService.remove('user_auth');
+            $location.path( "" );
+            location.reload();
+            // $scope.loggedIn = false;
+            // localStorageService.set('loggedIn', false);
+            $rootScope.$emit('loginStatus', false);
+            //toastr.info('Logout Success', 'You have logged out :(');
+        
+
+
 }]);
--- app/views/pages/pages.js
+++ app/views/pages/pages.js
@@ -8,6 +8,11 @@ angular.module('app.pages', ['ngRoute'])
             templateUrl: 'views/pages/pages.html',
             controller: 'pagesCtrl'
         });
+
+        $routeProvider.when('/admin/pages', {
+            templateUrl: 'views/pages/adminpages.html',
+            controller: 'adminPagesCtrl'
+        });
     }
 ])
 
@@ -15,28 +20,104 @@ angular.module('app.pages', ['ngRoute'])
     function($scope, $http, localStorageService, $rootScope) {
         $scope.title = "Pages";
         $scope.error = false;
-		
-        
-        
-		 $http({
-         url: "http://localhost:8080/pages",
-         method: 'GET',
-         dataType: 'json',
-         data: '',
-         headers: {
-             'Content-Type': 'application/json; charset=utf-8'
-         }
-			}).success(function(data, status, headers, config) {
+
+
+
+        $http({
+            url:  backend + "/pages",
+            method: 'GET',
+            dataType: 'json',
+            data: '',
+            headers: {
+                'Content-Type': 'application/json; charset=utf-8'
+            }
+        }).success(function(data, status, headers, config) {
+            $scope.pages = data.pages;
+            console.log(data.pages);
+
+        }).
+        error(function(data, status, headers, config) {
+            console.log(status);
+
+        });
+
+
+
+
+    }
+])
+    .controller('adminPagesCtrl', ['$scope', '$http', 'localStorageService', '$rootScope',
+        function($scope, $http, localStorageService, $rootScope) {
+            $scope.title = "Admin Pages";
+            $scope.error = false;
+
+
+
+            function loadPages() {
+                $http({
+                url: backend + "/pages",
+                method: 'GET',
+                dataType: 'json',
+                data: '',
+                headers: {
+                    'Content-Type': 'application/json; charset=utf-8'
+                }
+            }).success(function(data, status, headers, config) {
                 $scope.pages = data.pages;
                 console.log(data.pages);
-			
-		}).
-		error(function(data, status, headers, config) {
-            console.log(status);
-			
-		});
+
+            }).
+            error(function(data, status, headers, config) {
+                console.log(status);
+
+            });
+            }
+loadPages();
+
+            $scope.addSubpage = function(page, index) {
+                console.log(page);
+                console.log(index);
+                addPage(page, index);
+            }
+
+            $scope.addPage = function(page, index) {
+                
+
+                addPage(page, index);
+            }
+
+            function addPage(page, index) {
+
+                var slug = page.parent.replace(/\s+/g, '-').toLowerCase();
+                debugger;
+                $http({
+                    url: "http://localhost:8080/pages",
+                    method: 'POST',
+                    dataType: 'json',
+                    data: page.body,
+                    headers: {
+                        'Content-Type': 'application/json; charset=utf-8',
+                        'parentSlug': parent,
+                        'title': page.title,
+                        'description': page.description,
+                        'order':index,
+                        'permission': 'admin'
+
+                    }
+                }).success(function(data, status, headers, config) {
+                    $scope.pages = data.pages;
+                    console.log(data.pages);
+                    loadPages();
+
+                }).
+                error(function(data, status, headers, config) {
+                    console.log(status);
+
+                });
+            }
 
 
 
-	
-}]);
+
+        }
+    ]);
