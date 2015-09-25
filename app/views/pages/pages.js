8a54c66c3313b0b668c758973b923262f33d1f66
remony <sudouglas@dundee.ac.uk>
2015-09-25 12:36:33 +0100

    Pages implemented

--- app/views/pages/pages.js
+++ app/views/pages/pages.js
@@ -13,6 +13,11 @@ angular.module('app.pages', ['ngRoute'])
             templateUrl: 'views/pages/adminpages.html',
             controller: 'adminPagesCtrl'
         });
+
+        $routeProvider.when('/page/:slug', {
+            templateUrl: 'views/pages/page.html',
+            controller: 'pageCtrl'
+        });
     }
 ])
 
@@ -74,7 +79,7 @@ angular.module('app.pages', ['ngRoute'])
             }
 loadPages();
 
-            $scope.addSubpage = function(page, index) {
+            $scope.ad = function(page, index) {
                 console.log(page);
                 console.log(index);
                 addPage(page, index);
@@ -89,7 +94,6 @@ loadPages();
             function addPage(page, index) {
 
                 var slug = page.parent.replace(/\s+/g, '-').toLowerCase();
-                debugger;
                 $http({
                     url: "http://localhost:8080/pages",
                     method: 'POST',
@@ -97,7 +101,7 @@ loadPages();
                     data: page.body,
                     headers: {
                         'Content-Type': 'application/json; charset=utf-8',
-                        'parentSlug': parent,
+                        'parentSlug': slug,
                         'title': page.title,
                         'description': page.description,
                         'order':index,
@@ -106,7 +110,7 @@ loadPages();
                     }
                 }).success(function(data, status, headers, config) {
                     $scope.pages = data.pages;
-                    console.log(data.pages);
+                    console.log(data);
                     loadPages();
 
                 }).
@@ -120,4 +124,32 @@ loadPages();
 
 
         }
-    ]);
+    ])
+
+ .controller('pageCtrl', ['$scope', '$http', 'localStorageService', '$rootScope', '$routeParams',
+        function($scope, $http, localStorageService, $rootScope, $routeParams) {
+            $scope.title = $routeParams.slug;
+
+            getPage($routeParams.slug);
+
+            function getPage(slug) {
+                 $http({
+                url: backend + "/pages/" + slug,
+                method: 'GET',
+                dataType: 'json',
+                data: '',
+                headers: {
+                    'Content-Type': 'application/json; charset=utf-8'
+                }
+            }).success(function(data, status, headers, config) {
+                $scope.page = data.singlepage;
+                console.log(data.singlepage);
+
+            }).
+            error(function(data, status, headers, config) {
+                console.log(status);
+
+            });
+            }
+
+        }]);
