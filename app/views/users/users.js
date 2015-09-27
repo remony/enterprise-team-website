9d2e7b7a876f273f9e5c6d6c76dd929aac4e1561
Anna Solovjova <annsolovjova@gmail.com>
2015-09-27 15:24:43 +0100

    starting with the slider code

--- app/views/users/users.js
+++ app/views/users/users.js
@@ -283,7 +283,82 @@ angular.module('app.users', ['ngRoute'])
 }
 }
 
-}]);
+}])
+
+
+
+
+
+.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.when('/slider',{
+            templateUrl: 'views/slider/slider.html',
+            controller: 'SliderCtrl'
+        });
+    }
+])
+
+    .controller('SliderCtrl',  ['$scope', '$http', 'localStorageService', '$routeParams', function($scope, $http, localStorageService, $routeParams,config) {
+         $scope.slides = [
+            {image: '../assets/images/background.jpg', description: 'Image 00'},
+            {image: '../assets/images/logo.png', description: 'Image 01'},
+            
+        ];
+
+        $scope.currentIndex = 0;
+
+        $scope.setCurrentSlideIndex = function (index) {
+            $scope.currentIndex = index;
+        };
+
+        $scope.isCurrentSlideIndex = function (index) {
+            return $scope.currentIndex === index;
+        };
+
+        $scope.prevSlide = function () {
+            $scope.currentIndex = ($scope.currentIndex < $scope.slides.length - 1) ? ++$scope.currentIndex : 0;
+        };
+
+        $scope.nextSlide = function () {
+            $scope.currentIndex = ($scope.currentIndex > 0) ? --$scope.currentIndex : $scope.slides.length - 1;
+        };
+
+       
+
+    
+
+
+    }])
+     .animation('.slide-animation', function () {
+        return {
+            addClass: function (element, className, done) {
+                   if (className == 'ng-hide') {
+                    TweenMax.to(element, 0.5, {left: -element.parent().width(), onComplete: done });
+                }
+                else {
+                    done();
+                }
+            },
+            removeClass: function (element, className, done) {
+                if (className == 'ng-hide') {
+                    element.removeClass('ng-hide');
+                    TweenMax.set(element, { left: element.parent().width() });
+                    TweenMax.to(element, 0.5, {left: 0, onComplete: done });
+                }
+                else {
+                    done();
+                }
+            },
+            removeClass: function (element, className, done) {
+                if (className == 'ng-hide') {
+                    // ANIMATION CODE GOES HERE
+                }
+                else {
+                    done();
+                }
+            }
+        };
+    });
 
 
 
