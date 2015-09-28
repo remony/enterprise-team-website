ff6d892e72d606397d62b28dfb4bac4c55dcbb56
remony <sudouglas@dundee.ac.uk>
2015-09-28 18:21:58 +0100

    fixed issues

--- app/config.js
+++ app/config.js
@@ -1,17 +1,17 @@
 //Resused factory from previous projects
-var domain = 'http://46.101.32.73:8080/api';
-// var domain = 'http://localhost:8080'
+// var domain = 'http://46.101.32.73:8080/api';
+var domain = 'http://localhost:8080'
 
 //var port = '';
 
 var backend = domain;
 angular.module('app.config', [])
 
-    .factory('Configuration', function() {
-        return {
-            backend: backend
-        }
-    });
+.factory('Configuration', function() {
+    return {
+        backend: backend
+    }
+});
 
 
 
