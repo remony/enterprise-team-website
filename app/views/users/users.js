a9c33cb5cc8e104958df1bc64974964e581d3ef8
Anna Solovjova <annsolovjova@gmail.com>
2015-09-28 10:37:26 +0100

    no message

--- app/views/users/users.js
+++ app/views/users/users.js
@@ -299,66 +299,37 @@ angular.module('app.users', ['ngRoute'])
 ])
 
     .controller('SliderCtrl',  ['$scope', '$http', 'localStorageService', '$routeParams', function($scope, $http, localStorageService, $routeParams,config) {
+        
+        var imageNo=0;
          $scope.slides = [
             {image: '../assets/images/background.jpg', description: 'Image 00'},
             {image: '../assets/images/logo.png', description: 'Image 01'},
+            {image: '../assets/images/background.jpg', description: 'Image 02'},
+            {image: '../assets/images/logo.png', description: 'Image 03'},
             
         ];
 
-        $scope.currentIndex = 0;
+        $scope.image=$scope.slides[imageNo].image;
+   
+          setInterval(function(){
+            imageNo++;
+            console.log("hello"+imageNo);
+            if(imageNo===$scope.slides.length)
+            {
+                imageNo=0;
+            }
+           $scope.image=$scope.slides[imageNo].image;
+         
 
-        $scope.setCurrentSlideIndex = function (index) {
-            $scope.currentIndex = index;
-        };
+         }, 2000);
+        
 
-        $scope.isCurrentSlideIndex = function (index) {
-            return $scope.currentIndex === index;
-        };
-
-        $scope.prevSlide = function () {
-            $scope.currentIndex = ($scope.currentIndex < $scope.slides.length - 1) ? ++$scope.currentIndex : 0;
-        };
-
-        $scope.nextSlide = function () {
-            $scope.currentIndex = ($scope.currentIndex > 0) ? --$scope.currentIndex : $scope.slides.length - 1;
-        };
-
-       
 
     
 
 
-    }])
-     .animation('.slide-animation', function () {
-        return {
-            addClass: function (element, className, done) {
-                   if (className == 'ng-hide') {
-                    TweenMax.to(element, 0.5, {left: -element.parent().width(), onComplete: done });
-                }
-                else {
-                    done();
-                }
-            },
-            removeClass: function (element, className, done) {
-                if (className == 'ng-hide') {
-                    element.removeClass('ng-hide');
-                    TweenMax.set(element, { left: element.parent().width() });
-                    TweenMax.to(element, 0.5, {left: 0, onComplete: done });
-                }
-                else {
-                    done();
-                }
-            },
-            removeClass: function (element, className, done) {
-                if (className == 'ng-hide') {
-                    // ANIMATION CODE GOES HERE
-                }
-                else {
-                    done();
-                }
-            }
-        };
-    });
+    }]);
+   
 
 
 
